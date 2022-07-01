f7b5bbb4fdd00698c0a980ca05dadf7ac8a78435
Huynh Quang Vinh <huynhquangvinh01121999@gmail.com>
2022-07-01 16:40:52 +0700

    generate the first block when create blockchain

--- src/Wallet/Wallet.js
+++ src/Wallet/Wallet.js
@@ -1,37 +1,47 @@
-const crypto = require('crypto');
-const BlockChain = require('../BlockChain/BlockChain');
-const Transaction = require('../Transactions/Transactions')
+const crypto = require("crypto");
+const BlockChain = require("../BlockChain/BlockChain");
+const Transaction = require("../Transactions/Transactions");
 
 class Wallet {
-   constructor(difficuty) {
-      const keypair = crypto.generateKeyPairSync('rsa', {
-         modulusLength: 2048,
-         publicKeyEncoding: {
-            type: 'spki',
-            format: 'pem'
-         },
-         privateKeyEncoding: {
-            type: 'pkcs8',
-            format: 'pem'
-         }
-      });
+  constructor(difficuty) {
+    const keypair = crypto.generateKeyPairSync("rsa", {
+      modulusLength: 2048,
+      publicKeyEncoding: {
+        type: "spki",
+        format: "pem",
+      },
+      privateKeyEncoding: {
+        type: "pkcs8",
+        format: "pem",
+      },
+    });
 
-      this.publicKey = keypair.publicKey;
-      this.privateKey = keypair.privateKey;
-      this.difficuty = difficuty;
-   }
+    this.publicKey = keypair.publicKey;
+    this.privateKey = keypair.privateKey;
+    this.difficuty = difficuty;
+  }
 
-   sendMoney(data) {
-      const transaction = new Transaction(data.sender, data.receiver, data.amount, data.gasFee)
+  sendMoney(data) {
+    const transaction = new Transaction(
+      data.sender,
+      data.receiver,
+      data.amount,
+      data.gasFee
+    );
 
-      const sign = crypto.createSign("SHA256");
-      sign.update(transaction.toString()).end();
+    const sign = crypto.createSign("SHA256");
+    sign.update(transaction.toString()).end();
 
-      // tạo chữ kỹ số bằng khóa bí mật
-      const signature = sign.sign(this.privateKey);
+    // tạo chữ kỹ số bằng khóa bí mật
+    const signature = sign.sign(this.privateKey);
 
-      BlockChain.instance.addToBlockChain(transaction, this.publicKey, signature, this.difficuty)
-   }
+    BlockChain.instance.addToBlockChain(
+      transaction,
+      this.publicKey,
+      signature,
+      this.difficuty
+    );
+  }
 }
 
 module.exports = Wallet;
