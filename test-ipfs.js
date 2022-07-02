8811a3c80ed1bb40ca1d78b7dcffa4757fd1334e
VinhHuynh <huynhquangvinh01121999@gmail.com>
2022-07-03 01:29:22 +0700

    add link consult ipfs

--- test-ipfs.js
+++ test-ipfs.js
@@ -5,3 +5,9 @@ Hash.of(data).then(res => console.log(res))
 const CID = require('cids')
 const cid = new CID('QmT78zSuBmuS4z925WZfrqQ1qHaJ56DQaTfyMUF7F8ff5o')
 console.log(CID.isCID(cid));
+
+/**
+ * tham khảo ipfs tại đây:
+ * https://viblo.asia/p/nhung-goc-nhin-dau-tien-ve-ipfs-Az45b9PwlxY
+ * 
+ * */
